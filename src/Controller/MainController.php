d2c82890002006fedb325f58e91073290de308d8
rammroumm2K <ramybrz00@gmail.com>
2024-09-11 14:06:17 +0200

    v 0.3

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -12,7 +12,8 @@ class MainController extends AbstractController
     public function index(): Response
     {
         return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
+            'controller_name' => 'Rami',
+            'title' => 'HomePage',
         ]);
     }
 }
